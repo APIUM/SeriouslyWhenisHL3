ab6b491df2cb905efe8d45cdf2e0f85972d4ae74
apium <algytynan@gmail.com>
2017-06-12 00:55:26 +1000

    Started on multithreading for checking mail and other functions

--- SeriouslyWhenIsHl3.py
+++ SeriouslyWhenIsHl3.py
@@ -1,5 +1,6 @@
 import praw
 from credentials import *
+import threading
 
 # Main Reddit Auth, gives reddit.* 
 # Everything Is stored in 'credentials'
@@ -14,33 +15,36 @@ keywords = ['trump','and']
 # File config
 fCommented = open('commented', 'r')
 faCommented = open('commented', 'a')
-
 fDisabledUsers = open('disabledUsers', 'r')
-
 fDisabledSubs = open('disabledSubs', 'r')
 
-for i in range (0,len(subredditList)):
-    #subreddit = reddit.subreddit(subredditList[i])
-    subreddit = reddit.subreddit('all')
+# Main ugly loop
+subreddit = reddit.subreddit('all')
 
-    # assume you have a Subreddit instance bound to variable `subreddit`
-    for submission in subreddit.stream.submissions():
-        #print("___________")
-        print(submission.id)     # Output: the submission's ID
-        #print(submission.url)    # Output: the URL the submission points to
-        #print(submission.subreddit)
-                                 # or the submission's URL if it's a self post
-    
-    # GO to comments
-        submission.comments.replace_more(limit=0)
-        for comment in submission.comments.list():
+# GO to comments
+# This starts iterating through every comment 
+# Try is to close the files upon quit
+def replyToKeyword():
+    try:
+        for comment in subreddit.stream.comments():
             for key in keywords:
+                # Checks for keywords in the comment body
                 if key in comment.body.lower():
+                    # Checks for already processed comments
                     if comment.id not in fCommented.read():
+                        # Checks for opt-outed users
                         if comment.author.name not in fDisabledUsers.read():
+                            # Checks for opt-outed subs
                             if comment.subreddit.name not in fDisabledSubs.read():
                                 print("***********FOUND***********")
                                 print(key)
+                                print(comment.author.name)
                                 
+                                # Writes comment id to processed comments doc
                                 faCommented.write(comment.id+'\n')
-                        
+    except KeyboardInterrupt:
+        faCommented.close()
+        fDisabledUsers.close()
+        fDisabledSubs.close()
+
+threading.Thread().start()
