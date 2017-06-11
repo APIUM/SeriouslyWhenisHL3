61fa4ca257cc48df70128fb08ddd872f0092bdef
apium <algytynan@gmail.com>
2017-06-12 00:29:42 +1000

    Starting to do work

--- SeriouslyWhenIsHl3.py
+++ SeriouslyWhenIsHl3.py
@@ -7,12 +7,40 @@ from credentials import *
 # Authorising your app with reddit
 reddit = praw.Reddit(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,user_agent=USER_AGENT,username=USER,password=PASS)
 
-subreddit = reddit.subreddit('test')
+# Key Configuration
+subredditList = ['test', 'python']
+keywords = ['trump','and']
 
-# assume you have a Subreddit instance bound to variable `subreddit`
-for submission in subreddit.hot(limit=10):
-    print(submission.title)  # Output: the submission's title
-    print(submission.score)  # Output: the submission's score
-    print(submission.id)     # Output: the submission's ID
-    print(submission.url)    # Output: the URL the submission points to
-                             # or the submission's URL if it's a self post
+# File config
+fCommented = open('commented', 'r')
+faCommented = open('commented', 'a')
+
+fDisabledUsers = open('disabledUsers', 'r')
+
+fDisabledSubs = open('disabledSubs', 'r')
+
+for i in range (0,len(subredditList)):
+    #subreddit = reddit.subreddit(subredditList[i])
+    subreddit = reddit.subreddit('all')
+
+    # assume you have a Subreddit instance bound to variable `subreddit`
+    for submission in subreddit.stream.submissions():
+        #print("___________")
+        print(submission.id)     # Output: the submission's ID
+        #print(submission.url)    # Output: the URL the submission points to
+        #print(submission.subreddit)
+                                 # or the submission's URL if it's a self post
+    
+    # GO to comments
+        submission.comments.replace_more(limit=0)
+        for comment in submission.comments.list():
+            for key in keywords:
+                if key in comment.body.lower():
+                    if comment.id not in fCommented.read():
+                        if comment.author.name not in fDisabledUsers.read():
+                            if comment.subreddit.name not in fDisabledSubs.read():
+                                print("***********FOUND***********")
+                                print(key)
+                                
+                                faCommented.write(comment.id+'\n')
+                        
