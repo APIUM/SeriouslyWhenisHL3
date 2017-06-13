ac70a4dfe127a52b192b8ffe00b287bf7f4b8d68
apium <algytynan@gmail.com>
2017-06-13 16:34:13 +1000

    Cleaned print up for added sub to disabled

--- SeriouslyWhenIsHl3.py
+++ SeriouslyWhenIsHl3.py
@@ -123,8 +123,10 @@ def checkDisabledRequests():
                     print("SubArray: ")
                     print(subArray)
                     print("subToDisable: " + subToDisable)
+                    print("Moderators of %s that can match %s:" % (subToDiable, repr(item.author.name).replace("'",""))
                 for moderator in reddit.subreddit(subToDisable).moderator():
-                    print(moderator)
+                    if debug:
+                        print(moderator)
                     if moderator == repr(item.author.name).replace("'",""):
                         # Loads pickle file for disabled subs
                         disabledSubs = pickle.load(open("disabledSubs.p", "rb"))
