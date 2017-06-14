38cec7bc2ab4293b036ecdcd8a8662d2aee121b7
apium <algytynan@gmail.com>
2017-06-14 23:15:01 +1000

    Fixed it continuously adding to the sidebar, but it's still broken.

--- SeriouslyWhenIsHl3.py
+++ SeriouslyWhenIsHl3.py
@@ -14,13 +14,34 @@ reddit = praw.Reddit(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,user_agent=
 # Set True if you want to only check 1 sub
 useSubList = False
 # Use this if you don't want to comment
-dryRun = False
+dryRun = True
 # Prints lines for a lot of things
 debug = False
 # Comment is in commenting function below
 # botSub is the subreddit of the bot
 # Used to post to the sidebar
 botSub = 'WhenIsHl3'
+# Sidebar for sub
+sidebarBase = """
+We're back! And better than ever! It's great to see you here, unless you're a mod who doesn't want the bot on their sub, then it's just disapointing...
+
+But to get the bot off your sub just PM it:
+
+    !STOP@yourSubreddit
+
+where yourSubreddit is the name of your sub, eg: WhenIsHl3 if your sub is /r/WhenIsHL3
+
+
+
+You must be a mod of the subreddit for it to add it to the database!
+___
+
+
+For those interested, the start date for the bot was February 2015. But the revive date was June 2017. It's been a while....
+
+This bot is open source and avaliable on Github @ www.github.com/apium/SeriouslyWhenisHl3
+"""
+
 #************************#
 
 # Key Configuration
@@ -160,8 +181,8 @@ def updateSidebar():
     # Subreddit is defined in top config as 'botSub'
     # Settting is the subreddit settings object
     settings = reddit.get_settings(botSub)
-    sidebar_contents = settings['description']
-    sidebar_contents += "\n\nHalf Life 3 is set to release on %s!" % (releasestrf())
+    sidebar_contents = sidebarBase
+    sidebar_contents += "\n\n*Half Life 3 is set to release on %s!*" % (releasestrf())
     reddit.update_settings(reddit.get_subreddit(botSub), description=sidebar_contents)
 
 
